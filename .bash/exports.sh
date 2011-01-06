8d7994e32795c8c45d0d0fb434371f762b507d8f
Mike Busch <busch.michael@gmail.com>
2011-01-05 23:37:02 -0600

    added REE GC optimizations

--- .bash/exports.sh
+++ .bash/exports.sh
@@ -1,3 +1,4 @@
+export ARCHFLAGS='-arch x86_64'
 export EDITOR="$HOME/bin/mate -w"
 export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
 export M2_HOME=/usr/local/apache-maven/apache-maven-2.2.1
@@ -9,3 +10,10 @@ export MAVEN_PATHS="$M2:/usr/share/maven/bin"
 export PATH="$HOME/bin:$HOMEBREW_PATHS:/opt/local/bin:/opt/local/sbin:$MAVEN_PATHS:$JAVA_HOME/bin:/opt/subversion/bin:$PATH"
 export CDPATH=.
 export LSCOLORS=gxfxcxdxbxegedabagacad
+
+#Ruby Enterprise Edition
+export RUBY_HEAP_MIN_SLOTS=1000000
+export RUBY_HEAP_SLOTS_INCREMENT=1000000
+export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
+export RUBY_GC_MALLOC_LIMIT=1000000000
+export RUBY_HEAP_FREE_MIN=500000
