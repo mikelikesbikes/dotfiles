f52b0563455be94fe316316d0f53f8d85e35811b
Mike Busch <busch.michael@gmail.com>
2011-01-21 16:11:48 -0600

    added rails 2/3 script aliases

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -1,7 +1,41 @@
+# rails 3 shortcut 'r'
+alias r='rails'
+
+# launching console/server
+sc () {
+  if [ -f ./script/rails ]; then 
+    rails c $@
+  else
+    ./script/console $@
+  fi
+}
+
+sg () {
+  if [ -f ./script/rails ]; then
+    rails g $@
+  else
+    ./script/generate $@
+  fi
+}
+
+ss () {
+  if [ -f ./script/rails ]; then 
+    rails s $@
+  else
+    ./script/server $@
+  fi
+}
+
+sspe () {
+  if [ -f ./script/rails ]; then 
+    sudo rails s -p80 $@
+  else
+    sudo ./script/server -p80 $@
+  fi
+}
+
+
 alias ls="ls -G"
-alias sc="./script/console"
-alias ss="./script/server"
-alias sg="./script/generate"
 alias lop='sudo lsof -i -P | grep -i "listen"'
 alias mci="mvn clean install"
 alias mcp="mvn clean package"
