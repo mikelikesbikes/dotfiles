8ecc1179b314445a798ca73fce1dfbc292ddc795
Mike Busch <busch.michael@gmail.com>
2012-02-27 11:40:00 -0600

    remove ack completion script, because it's broken

--- bash/completion.sh
+++ bash/completion.sh
@@ -2,5 +2,4 @@ source ~/.bash/completion/bash.sh
 source ~/.bash/completion/ssh.sh
 
 source /usr/local/etc/bash_completion.d/git-completion.bash
-source /usr/local/etc/bash_completion.d/ack.bash_completion.sh
 
