026188099c4d449584bdd432f1cb7974429cfff5
Mike Busch <busch.michael@gmail.com>
2012-02-27 11:36:08 -0600

    source the brewed git completion script. remove the vendored one.

--- bash/completion.sh
+++ bash/completion.sh
@@ -1,4 +1,6 @@
 source ~/.bash/completion/bash.sh
-source ~/.bash/completion/git.sh
 source ~/.bash/completion/ssh.sh
+
+source /usr/local/etc/bash_completion.d/git-completion.bash
+source /usr/local/etc/bash_completion.d/ack.bash_completion.sh
 
