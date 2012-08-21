0a672b9045d542a03ad17857a1eed685f2f0231f
Mike Busch <busch.michael@gmail.com>
2012-08-21 08:28:54 -0500

    refactor exports

--- bash/exports.sh
+++ bash/exports.sh
@@ -1,28 +1,35 @@
-export ARCHFLAGS='-arch x86_64'
-export CC=/usr/bin/gcc-4.2
-export EDITOR="/usr/bin/vim"
-export BUNDLER_EDITOR="mvim"
-export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
-#export M2_HOME=/usr/local/apache-maven/apache-maven-2.2.1
-#export MAVEN_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xms256m -Xmx512m -XX:MaxPermSize=128m"
-# export MAVEN_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=3998,suspend=n,server=y -Xms256m -Xmx512m -XX:MaxPermSize=256m"
-#export M2=$M2_HOME/bin
-export HOMEBREW_PATHS="/usr/local/bin:/usr/local/sbin"
-#export MAVEN_PATHS="$M2:/usr/share/maven/bin"
+# Architecture/Compilers
+  export ARCHFLAGS='-arch x86_64'
+  export CC=/usr/bin/gcc-4.2
 
-export STORM_BIN="$HOME/projects/storm/storm-0.7.4/bin"
+# Editor
+  export EDITOR="/usr/bin/vim"
+  export BUNDLER_EDITOR="mvim"
 
-export PATH="$HOME/bin:$HOMEBREW_PATHS:/opt/local/bin:/opt/local/sbin:/opt/nginx/sbin/:$STORM_BIN:$JAVA_HOME/bin:/opt/subversion/bin:$PATH"
-export CDPATH=.
-export LSCOLORS=gxfxcxdxbxegedabagacad
-export DOTFILES=~/dotfiles
+# Miscellaneous
+  export DOTFILES=$HOME/dotfiles
+  export CDPATH=.:$HOME:$HOME/projects
+  export LSCOLORS=gxfxcxdxbxegedabagacad
 
 #Ruby Enterprise Edition
-export RUBY_HEAP_MIN_SLOTS=1000000
-export RUBY_HEAP_SLOTS_INCREMENT=1000000
-export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
-export RUBY_GC_MALLOC_LIMIT=1000000000
-export RUBY_HEAP_FREE_MIN=500000
+  export RUBY_HEAP_MIN_SLOTS=1000000
+  export RUBY_HEAP_SLOTS_INCREMENT=1000000
+  export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
+  export RUBY_GC_MALLOC_LIMIT=1000000000
+  export RUBY_HEAP_FREE_MIN=500000
 
 #JRuby
-export JRUBY_OPTS="--1.9"
+  export JRUBY_OPTS="--1.9"
+
+# Java stuff
+  export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
+  #export M2_HOME=/usr/local/apache-maven/apache-maven-2.2.1
+  #export MAVEN_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xms256m -Xmx512m -XX:MaxPermSize=128m"
+  #export MAVEN_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=3998,suspend=n,server=y -Xms256m -Xmx512m -XX:MaxPermSize=256m"
+  #export M2=$M2_HOME/bin
+
+# Path stuff (at the bottom)
+  export HOMEBREW_PATH="/usr/local/bin:/usr/local/sbin"
+  export STORM_PATH="$HOME/projects/storm/storm-0.7.4/bin"
+  export PATH="$HOME/bin:$HOMEBREW_PATH:/opt/local/bin:/opt/local/sbin:/opt/nginx/sbin/:$STORM_PATH:$JAVA_HOME/bin:/opt/subversion/bin:$PATH"
+
