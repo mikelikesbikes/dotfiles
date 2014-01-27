5a99beb25979d6de007e859dceaad40ac3fe1edb
Mike Busch <busch.michael@gmail.com>
2014-01-27 13:57:30 -0600

    fix issue with ruby version not updating
    missing \ before the $(...) will cause the $(...) to expand immediately
    rather than each time the prompt is rendered.

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -4,7 +4,7 @@ source ~/.bash/colors.sh
 RUBY=$'\xf0\x9f\x94\xbb'
 ARROW=$'\xe2\x9e\x9c'
 
-scm_ps1() {
+__scm_ps1() {
     local s=
     if [[ -d ".svn" ]] ; then
         s=\ \(svn:$(svn info | sed -n -e '/^Revision: \([0-9]*\).*$/s//\1/p' )\)
@@ -15,11 +15,11 @@ scm_ps1() {
     echo -n "$s"
 }
 
-rbenv_ruby_version() {
+__rbenv_ruby_version() {
   if which rbenv > /dev/null; then
     rbenv version | sed 's/\([^ ]*\).*/\1/'
   fi
 }
 
 export GIT_PS1_SHOWDIRTYSTATE=true
-export PS1="$Blue\u$ResetColor_Off:$BCyan\w$Green\$(__git_ps1) $IRed$RUBY $(rbenv_ruby_version)$ResetColor_Off\n$Purple$ARROW  $ResetColor_Off"
+export PS1="$Blue\u$ResetColor_Off:$BCyan\w$Green\$(__git_ps1) $IRed$RUBY \$(__rbenv_ruby_version)$ResetColor_Off\n$Purple$ARROW  $ResetColor_Off"
