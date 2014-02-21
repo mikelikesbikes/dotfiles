ea836cebbe9108b913068920d21600c861a85d46
Mike Busch <busch.michael@gmail.com>
2014-02-21 10:49:19 -0600

    use bash-completion package for completion scripts

--- bash/completion.sh
+++ bash/completion.sh
@@ -1,5 +1,8 @@
-source ~/.bash/completion/bash.sh
-source ~/.bash/completion/ssh.sh
-
-source /usr/local/etc/bash_completion.d/git-completion.bash
+# I'm using the bash-completion package (brew install bash-completion)
+# to provide a ton of extra completion stuff. This also loads completions
+# that are included in $(brew --prefix)/etc/bash_completion.d that are
+# installed along with other packages (e.g. git and leinengen)
+if [ -f $(brew --prefix)/etc/bash_completion ]; then
+  . $(brew --prefix)/etc/bash_completion
+fi
 
--- bash/completion/ssh.sh
+++
@@ -1,1 +0,0 @@
-complete -W "$(echo $(grep '^ssh ' $HOME/.bash_history | sort -u | sed 's/^ssh //'))" ssh
